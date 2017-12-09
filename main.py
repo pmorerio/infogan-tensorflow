83f6bafec54ea86937d46efb1e3c6cab2e5513f2
Pietro Morerio <pietro.morerio@gmail.com>
2017-12-09 14:29:06 +0100

    lambda_cat flag

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ flags.DEFINE_string('gpu', '0', "'GPU id'")
 flags.DEFINE_string('mode', 'train', "'pretrain', 'train' or 'eval'")
 flags.DEFINE_string('model_save_path', 'model', "directory for saving the model")
 flags.DEFINE_string('cat_codes', '0', "number of categorcial codes")
+flags.DEFINE_string('lambda_cat', '0.1', "number of categorcial codes")
 FLAGS = flags.FLAGS
 
 def main(_):
     
     with tf.device('/gpu:'+FLAGS.gpu):
 
-	model = infogan(mode=FLAGS.mode, n_cat_codes=int(FLAGS.cat_codes), learning_rate=0.0005)
+	model = infogan(mode=FLAGS.mode, n_cat_codes=int(FLAGS.cat_codes), 
+			lambda_cat=FLAGS.lambda_cat, learning_rate=0.0001)
 	solver = Solver(model, model_save_path=FLAGS.model_save_path)
 	
 	if FLAGS.mode == 'train':
